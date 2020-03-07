a946004b228aac947761f9314dba561bf83ba908
Caio Henrique <caiohfmoura@gmail.com>
2020-03-06 22:21:47 -0300

    first java class

---
+++ OlaMundoJava/Ex2.java
@@ -0,0 +1,8 @@
+public class Ex2
+{
+   public static void main(String[] args){
+       double montanteConversao = 1050.00;
+       double cotacaoDolar = 4.5;
+       double valorFinal = montanteConversao / cotacaoDolar;
+    }
+}
--- OlaMundoJava/OlaMundoJava.java
+++ OlaMundoJava/OlaMundoJava.java
@@ -2,6 +2,27 @@
 public class OlaMundoJava
 {
     public static void main(String[] args){
-        System.out.println("Meu primeiro código em Java na Fib");
+        
+        float valorSalario = 1300.45f; //valores decimais
+        int numeroDependentes = 5;  //valores inteiros
+        String nomeCidade = "Bauru"; //valores string
+        char letra = 'B'; //valors caracter
+        boolean temSaldo = true; //true e falso
+        
+        double valor1 = 10.20;
+        double valor2 = 30.20;
+        double soma = valor1 + valor2 ;
+        
+        if(soma > 100){
+             System.out.println("A soma de " + valor1+ " mais " + valor2+ " é maior que cem");
+        }
+        
+        else{
+            System.out.println("A soma de " + valor1+ " mais " + valor2+ " é menor que cem");
+        }
+        
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
