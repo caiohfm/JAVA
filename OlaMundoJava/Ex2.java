c98f9a48a1058203dc39deabf57b9ed2375522a4
Caio Henrique <caiohfmoura@gmail.com>
2020-03-13 20:59:39 -0300

    class exercises finished

--- OlaMundoJava/Ex2.java
+++ OlaMundoJava/Ex2.java
@@ -4,5 +4,15 @@ public class Ex2
        double montanteConversao = 1050.00;
        double cotacaoDolar = 4.5;
        double valorFinal = montanteConversao / cotacaoDolar;
+       
+       if(valorFinal<1000 && valorFinal>100){
+           System.out.println("O valor final é: "+valorFinal * 0.95);
+        }
+       else if(valorFinal>1000){
+           System.out.println("O valor final é: "+valorFinal * 0.9);
+        }
+        else{
+           System.out.println("O valor final é: "+valorFinal);
+        }
     }
 }
